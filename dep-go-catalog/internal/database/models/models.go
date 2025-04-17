f37da39885d26c8e1903f0d70141d7eb080e4a29
QuiGon189 <leshka189@gmail.com>
2025-04-17 23:05:12 +0300

    update models catalog

--- dep-go-catalog/internal/database/models/models.go
+++ dep-go-catalog/internal/database/models/models.go
@@ -1,52 +1,52 @@
-package models
+mackage models
 
 import "time"
 
 type Specialty struct {
 	BaseModel
-	Code      string  `gorm:"size:8"`
-	Name      string  `gorm:"size:100"`
-	ShortName string  `gorm:"size:3"`
+	Code      string  `gorm:"size:8;unique;no null"`
+	Name      string  `gorm:"size:100;unique;no null"`
+	ShortName string  `gorm:"size:3, no null"`
 	Groups    []Group `gorm:"foreignKey:SpecID"`
 }
 
 type Group struct {
 	BaseModel
-	Number         uint
-	Year           uint
+	Number         uint `gorm:"no null"`
+	Year           uint `gorm:"no null"`
 	ClassTeacherID uint
-	SpecID         uint
-	Students       []Student `gorm:"foreignKey:GroupID"`
-	Disciplines 
+	SpecID         uint         `gorm:"no null"`
+	Students       []Student    `gorm:"foreignKey:GroupID"`
+	Disciplines    []Discipline `gorm:"foreignKey:GroupID"`
 }
 
 type Student struct {
 	BaseModel
-	FullName    string
+	FullName    string `gorm:"size:255;no null"`
 	GroupID     uint
-	Birthdate   time.Time
-	PhoneNumber string
+	Birthdate   time.Time `gorm:"no null"`
+	PhoneNumber string    `gorm:"size:16"`
 }
 
 type Teacher struct {
 	BaseModel
-	FullName    string
-	Birthdate   time.Time
-	PhoneNumber string
+	FullName    string    `gorm:"size:255;no null"`
+	Birthdate   time.Time `gorm:"no null"`
+	PhoneNumber string    `gorm:"size:16"`
 }
 
 type Discipline struct {
 	BaseModel
-	Code     string
-	Name     string
+	Code     string `gorm:"size:10;no null"`
+	Name     string `gorm:"size:255;no null"`
 	GroupID  uint
-	Semester uint
-	Hours    uint
+	Semester uint `gorm:"no null"`
+	Hours    uint `gorm:"no null"`
 }
 
 type Audience struct {
 	BaseModel
-	Number    uint
-	Name      string
+	Number    uint `gorm:"no null"`
+	Name      string `gorm:"size:255;no null"`
 	TeacherID uint
 }

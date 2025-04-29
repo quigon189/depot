1efcc2dae324ab19edc3323466e4a44ee2abaacb
QuiGon189 <leshka189@gmail.com>
2025-04-30 00:29:24 +0300

    add go-catalog in compose and makefile

--- Makefile
+++ Makefile
@@ -5,13 +5,14 @@ dev-start: #Старт окружения разработчика
 	podman image prune -f
 	sleep 10
 	http :18080/add name=laa email=laa@gmail.com password=ghjgecr123
+	./dep-go-catalog/test/test.sh 18081
 	@echo "Depot server: http://$(ADDR):5000"
-	@echo "Swagger Editor: http://$(ADDR):8080"
-	@echo "SwaggerUI: http://$(ADDR):8081"
+	@echo "dep-go-auth: http://$(ADDR):18080"
+	@echo "dep-go-catalog: http://$(ADDR):18081"
 
 dev-stop:
 	podman-compose down
 
 dev-update:
-	podman rmi localhost/depot_flask-front:latest localhost/depot_go-auth:latest || true
+	podman rmi localhost/depot_flask-front:latest localhost/depot_go-auth:latest localhost/depot_go-catalog:latest || true
 	podman-compose build
--- dep-go-catalog/test/test.sh
+++ dep-go-catalog/test/test.sh
@@ -1,13 +1,15 @@
 #! /bin/bash
 
-http -v POST :8082/specialties Content-Type:application/json < <(echo '[{"name":"Сетевое и системное администрирование", "code":"09.02.06", "short_name":"СА"}]')
+port=$1
 
-http -v POST :8082/teachers Content-Type:application/json < <(echo '[{"first_name":"Иван", "middle_name":"Иванович", "last_name":"Иванов", "birth_date":"01.01.1990"}]')
+http -v POST :$port/specialties Content-Type:application/json < <(echo '[{"name":"Сетевое и системное администрирование", "code":"09.02.06", "short_name":"СА"}]')
 
-http -v POST :8082/groups Content-Type:application/json < <(echo '[{"number":501, "year_formed":2023, "spec_id":1, "class_teacher_id":1}]')
+http -v POST :$port/teachers Content-Type:application/json < <(echo '[{"first_name":"Иван", "middle_name":"Иванович", "last_name":"Иванов", "birth_date":"01.01.1990"}]')
 
-http -v POST :8082/students Content-Type:application/json < <(echo '[{"first_name":"Студент", "middle_name":"Студентович", "last_name":"Студентов", "birth_date":"02.02.2002", "group_id":1}]')
+http -v POST :$port/groups Content-Type:application/json < <(echo '[{"number":501, "year_formed":2023, "spec_id":1, "class_teacher_id":1}]')
 
-http -v POST :8082/disciplines Content-Type:application/json < <(echo '[{"code":"ОП.10", "name":"Электротехника", "semester":1, "hours":70, "group_id":1}]')
+http -v POST :$port/students Content-Type:application/json < <(echo '[{"first_name":"Студент", "middle_name":"Студентович", "last_name":"Студентов", "birth_date":"02.02.2002", "group_id":1}]')
 
-http -v POST :8082/classes Content-Type:application/json < <(echo '[{"number":409, "name":"Операционные системы", "type":"Лаборатория", "teacher_id":1}]')
+http -v POST :$port/disciplines Content-Type:application/json < <(echo '[{"code":"ОП.10", "name":"Электротехника", "semester":1, "hours":70, "group_id":1}]')
+
+http -v POST :$port/classes Content-Type:application/json < <(echo '[{"number":409, "name":"Операционные системы", "type":"Лаборатория", "teacher_id":1}]')
